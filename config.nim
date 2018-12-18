9abb619a216cdd9094361b77fe90a864c3aae93b
Akira Komamura <akira.komamura@gmail.com>
2018-12-18 23:54:31 +0900

    Add more commands and keybindings
    These are not my favourites at all. I stole them from the internet,
    so I will tweak them later.

--- config.nim
+++ config.nim
@@ -7,16 +7,47 @@ import herbstluftwm/main
 
 let
   myMod = mod4
+  terminal = "alacritty"
+  resizeStep = 0.05
+
+# TODO: Set attributes
+# TODO: Set rules
 
 herbstluftwmMain(keybindings = @[
     (myMod p, @["spawn", "rofi", "-show", "run"]),
-    (myMod ret, @["spawn", "alacritty"]),
+    (myMod shift c, closeWindow),
+    (myMod ret, @["spawn", terminal]),
+    (myMod j, focusDown),
+    (myMod k, focusUp),
+    (myMod h, focusLeft),
+    (myMod l, focusRight),
+    (myMod shift j, shiftDown),
+    (myMod shift k, shiftUp),
+    (myMod shift h, shiftLeft),
+    (myMod shift l, shiftRight),
+    (myMod ctrl j, resizeDown resizeStep),
+    (myMod ctrl k, resizeUp resizeStep),
+    (myMod ctrl h, resizeLeft resizeStep),
+    (myMod ctrl l, resizeRight resizeStep),
     (myMod shift r, wmReload),
     (myMod shift q, wmQuit),
+    (myMod u, splitFrame(dirBottom, 0.5)),
+    (myMod o, splitFrame(dirRight, 0.5)),
+    (myMod x, removeFrame),
+    (myMod t, toggleFloating),
+    (myMod f, toggleFullscreen),
+    (myMod shift t, toggleTiling),
+    (myMod ctrl space, explodeFrame),
+    (myMod space, @["cycle_layout", "1"]),
+    (myMod z, @["jumpto", "urgent"]),
     (myMod num1, use_index 1),
     (myMod num2, use_index 2),
     (myMod num3, use_index 3),
     (myMod num4, use_index 4),
+    (myMod shift num1, move_index 1),
+    (myMod shift num2, move_index 2),
+    (myMod shift num3, move_index 3),
+    (myMod shift num4, move_index 4),
     (myMod right, use_index next),
     (myMod left, use_index prev)
 ],
--- herbstluftwm/command.nim
+++ herbstluftwm/command.nim
@@ -5,6 +5,12 @@ let
   # Used in use_index
   next* = "+1"
   prev* = "-1"
+  # directions
+  dirBottom* = "bottom"
+  dirDown* = "down"
+  dirRight* = "right"
+  dirUp* = "up"
+  dirLeft* = "left"
 
 let
   wmReload* = @["reload"]
@@ -16,6 +22,12 @@ proc use_index*(index: string): Command =
 proc use_index*(index: int): Command =
   @["use_index", $index]
 
+proc move_index*(index: string): Command =
+  @["move_index", index]
+
+proc move_index*(index: int): Command =
+  @["move_index", $index]
+
 # proc spawn*(cmd: seq[string]): Command =
 #   @["spawn"] & cmd
 
@@ -24,3 +36,36 @@ proc use_index*(index: int): Command =
 
 # proc spawn*(cmd: string): Command =
 #   @["spawn", cmd]
+
+proc focus*(direction: string): Command =
+  @["focus", direction]
+
+proc shiftWindow*(direction: string): Command =
+  @["shift", direction]
+
+proc splitFrame*(direction: string, ratio: float): Command =
+  @["split", direction, $ratio]
+
+proc resizeFrame*(direction: string, ratio: float): Command =
+  @["resize", direction, $ratio]
+
+proc resizeLeft*(ratio: float): Command = resizeFrame("left", ratio)
+proc resizeRight*(ratio: float): Command = resizeFrame("right", ratio)
+proc resizeUp*(ratio: float): Command = resizeFrame("up", ratio)
+proc resizeDown*(ratio: float): Command = resizeFrame("down", ratio)
+
+let
+  focusLeft* = focus "left"
+  focusRight* = focus "right"
+  focusDown* = focus "down"
+  focusUp* = focus "up"
+  shiftLeft* = shiftWindow "left"
+  shiftRight* = shiftWindow "right"
+  shiftDown* = shiftWindow "down"
+  shiftUp* = shiftWindow "up"
+  closeWindow* = @["close"]
+  explodeFrame* = @["split", "explode"]
+  removeFrame* = @["remove"]
+  toggleFloating* = @["floating", "toggle"]
+  toggleTiling* = @["pseudotile", "toggle"]
+  toggleFullscreen* = @["fullscreen", "toggle"]
